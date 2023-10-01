f07bd69cb18eb09ed0fcf448be2cdabcdbc0ed9b
Merna Abdallah <91502893+MernaAbdallah@users.noreply.github.com>
2023-10-01 22:27:03 +0300

    Add files via upload

---
+++ src/main/java/com/example/myadhd/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myadhd;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.myadhd.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+     ActivityMainBinding b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        b = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
+        replaceFragment(new HomeFragment());
+        b.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.routine:
+                    replaceFragment(new RoutineFragment());
+                    break;
+                case R.id.tasks:
+                    replaceFragment(new TasksFragment());
+                    break;
+            }
+
+
+            return true;
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
+
+
+}
